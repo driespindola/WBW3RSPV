b39a07cc2af40175e598e9387cd0946e0c482b2a
Natalie <natalieg@Natalies-MacBook-Air-3.local>
2022-08-06 13:10:18 +0300

    second commit

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -1,8 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
-
-// Import this file to use console.log
-import "hardhat/console.sol";
+pragma solidity ^0.8.0;
 
 contract Web3RSVP {
 
